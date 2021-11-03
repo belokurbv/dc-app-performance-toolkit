feb7a6873172e25ed584ea69b858d031b6d8c734
Bohdan Belokur <bohdan.belokur@dxc.com>
2021-11-03 14:24:57 +0200

    Adding custom actions

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -1,6 +1,10 @@
 import random
+import string
+import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
@@ -10,35 +14,69 @@ from util.conf import JIRA_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
+    actions = ActionChains(webdriver)
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    userId = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+
+
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.set_credentials(username=username, password=password)
+            if login_page.is_first_login():
+                login_page.first_login_setup()
+            if login_page.is_first_login_second_page():
+                login_page.first_login_second_page_setup()
+            login_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
+
 
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
+        @print_timing("selenium_app_custom_action:my_subscriptions_page")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/MySubscriptionsAction.jspa")
+            page.wait_until_visible((By.ID, "mysubscriptionsList"))
+            tabs = page.get_elements((By.CLASS_NAME, "release-report-tab"))
+            tabs[1].click()
+            page.wait_until_visible((By.ID, "mysubscriptionsList"))
+            tabs[2].click()
+            page.wait_until_visible((By.ID, "mysubscriptionsList"))
+            tabs[3].click()
+            page.wait_until_visible((By.ID, "mysubscriptionsList"))
+            tabs[4].click()
+            page.wait_until_visible((By.ID, "mysubscriptionsList"))
+        sub_measure()
+
+        @print_timing("selenium_app_custom_action:subscriptions_page")
+        def sub_measure():
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/SubscriptionsAction!default.jspa")
+            page.wait_until_visible((By.ID, "subscriptionsList"))
+            tabs = page.get_elements((By.CLASS_NAME, "release-report-tab"))
+            tabs[1].click()
+            page.wait_until_visible((By.ID, "subscriptionsList"))
+            tabs[2].click()
+            page.wait_until_visible((By.ID, "subscriptionsList"))
+            tabs[3].click()
+            page.wait_until_visible((By.ID, "subscriptionsList"))
+            tabs[4].click()
+            page.wait_until_visible((By.ID, "subscriptionsList"))
+        sub_measure()
+
+        @print_timing("selenium_app_custom_action:search_by_jql")
+        def sub_measure():
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/SubscriptionsAction!default.jspa")
+            page.wait_until_visible((By.ID, "tl-subscr-filters"))
+            page.get_element((By.ID, "tl-subscr-filters")).click()
+            page.wait_until_visible((By.ID, "tl-subscr-filters-jql-like"))
+            jqlinput = page.get_element((By.ID, "tl-subscr-filters-jql-like"))
+            jqlinput.send_keys("project = 10201")
+            page.get_element((By.CLASS_NAME, "tl-subscr-button-primary")).click()
+            page.wait_until_visible((By.ID, "subscriptionsList"))
         sub_measure()
     measure()
 
