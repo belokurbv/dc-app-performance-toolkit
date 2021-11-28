e6ede597d7560283797bcffa56d9e5e50509eb55
Bohdan Belokur <bohdan.belokur@dxc.com>
2021-11-28 19:02:31 +0200

    Initial commit

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -1,6 +1,10 @@
 import random
+import string
+import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
@@ -10,35 +14,63 @@ from util.conf import JIRA_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
+    actions = ActionChains(webdriver)
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    userId = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(12))
+
+
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.set_credentials(username=username, password=password)
+            if login_page.is_first_login():
+                login_page.first_login_setup()
+            if login_page.is_first_login_second_page():
+                login_page.first_login_second_page_setup()
+            login_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
+
 
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
+        @print_timing("selenium_app_custom_action:view_entries")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/catalogDictionary.jspa")
+        sub_measure()
+
+        @print_timing("selenium_app_custom_action:create_entry")
+        def sub_measure():
+            page.wait_until_visible((By.ID, "add-record"))
+            addButton = page.get_element((By.ID, "add-record"))
+            addButton.click()
+            page.wait_until_visible((By.ID, "clients-custom_dict_1_name"))
+            entryInput = page.get_element((By.ID, "clients-custom_dict_1_name"))
+            entryInput.clear()
+            entryInput.send_keys(userId + "catalogtest")
+            page.wait_until_visible((By.CLASS_NAME, "tl-catalog-save-cl-btn-"))
+            addButton = page.get_element((By.CLASS_NAME, "tl-catalog-save-cl-btn-"))
+            addButton.click()
+        sub_measure()
+
+        @print_timing("selenium_app_custom_action:create_issue_with_custom_catalog_field")
+        def sub_measure():
+            page.wait_until_visible((By.ID, "create_link"))
+            createButton = page.wait_until_visible((By.ID, "create_link"))
+            createButton.click()
+            page.wait_until_visible((By.ID, "summary"))
+            summaryInput = page.get_element((By.ID, "summary"))
+            summaryInput.clear()
+            summaryInput.send_keys(userId + "catalogtest")
+            page.wait_until_visible((By.ID, "tl-ctl-dict-picker-customfield_11100-field"))
+            customInput = page.get_element((By.ID, "tl-ctl-dict-picker-customfield_11100-field"))
+            customInput.clear()
+            customInput.send_keys("test")
+            submitButton = page.wait_until_visible((By.ID, "create-issue-submit"))
+            submitButton.click()
         sub_measure()
     measure()
 
