eb0b32f86d835144b6815c74e3de6bc3a2ce2397
Bohdan Belokur <bohdan.belokur@dxc.com>
2021-12-11 17:51:14 +0200

    Calendar for Jira tests

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -1,6 +1,11 @@
 import random
+import string
+import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
@@ -10,35 +15,69 @@ from util.conf import JIRA_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
+    actions = ActionChains(webdriver)
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    userId = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+
+
+    @print_timing("selenium_app_specific_user_login")
+    def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.set_credentials(username=username, password=password)
+            if login_page.is_first_login():
+                login_page.first_login_setup()
+            if login_page.is_first_login_second_page():
+                login_page.first_login_second_page_setup()
+            login_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+    measure()
+
 
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
+        @print_timing("selenium_app_custom_action:view_calendar")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/TeamleadCalendarAction.jspa")
+            page.wait_until_visible((By.ID, "calendarz"))
+        sub_measure()
+
+        @print_timing("selenium_app_custom_action:create_issue")
+        def sub_measure():
+            page.wait_until_visible((By.XPATH, '//*[@id="calendar-dd"]/div[2]/div/table/tbody/tr/td/div/div/div[2]/div[2]/table/thead/tr/td[4]'))
+            date = page.get_element((By.XPATH, '//*[@id="calendar-dd"]/div[2]/div/table/tbody/tr/td/div/div/div[2]/div[2]/table/thead/tr/td[4]'))
+            actions.move_to_element(date).perform();
+            time.sleep(1)
+            actions.click().perform();
+            page.wait_until_visible((By.ID, "create-calendar-issue"))
+            createbtn = page.get_element((By.ID, "create-calendar-issue"))
+            createbtn.click()
+            page.wait_until_visible((By.ID, "summary"))
+            summaryInput = page.get_element((By.ID, "summary"))
+            summaryInput.clear()
+            summaryInput.send_keys(userId + "calendartest")
+            select = Select(webdriver.find_element_by_id("resolution"))
+            select.select_by_value('10030')
+            submitButton = page.get_element((By.ID, "create-issue-submit"))
+            submitButton.click()
+        sub_measure()
+
+        @print_timing("selenium_app_custom_action:create_entry")
+        def sub_measure():
+            page.wait_until_visible((By.XPATH, '//*[@id="calendar-dd"]/div[2]/div/table/tbody/tr/td/div/div/div[2]/div[2]/table/thead/tr/td[7]'))
+            date2 = page.get_element((By.XPATH, '//*[@id="calendar-dd"]/div[2]/div/table/tbody/tr/td/div/div/div[2]/div[2]/table/thead/tr/td[7]'))
+            actions.move_to_element(date2).click().perform();
+            page.wait_until_visible((By.ID, "create-non-jira-event"))
+            createbtn = page.get_element((By.ID, "create-non-jira-event"))
+            createbtn.click()
+            page.wait_until_visible((By.ID, "event-name"))
+            nameInput = page.get_element((By.ID, "event-name"))
+            nameInput.clear()
+            nameInput.send_keys(userId + "calendartest")
+            submitButton = page.get_element((By.XPATH, '//*[@id="choosePopup"]/div/div[2]/button'))
+            submitButton.click()
         sub_measure()
     measure()
 
